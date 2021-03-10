50a4f7f1bdf54eb794ec8327df87ace1b76589f1
Taras Kulyk <taras.kulyk1@gmail.com>
2021-03-10 23:45:26 +0200

    Add model && migration for posts

---
+++ app/Models/Post.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Post extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+    ];
+
+    /**Start Relations */
+    /**End Relations */
+
+    /**Start Scopes*/
+    /**End Scopes */
+
+    /**Start Mutators*/
+    /**End Mutators */
+
+    /**Start Helper*/
+    /**End Helper*/
+}
